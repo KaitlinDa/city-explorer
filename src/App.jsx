f0b2223dbacea065f409ff95a12dc4d6f865095c
KAITLIN DAVIS <KAITLIN.A.DAVIS1996@GMAIL.COM>
2023-12-06 17:15:17 -0600

    changed url

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,28 @@ import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
+
 export default function App() {
   const [location, setLocation] = useState({display_name: "info about ???"});
   const [searchQuery, setSearchQuery] = useState('');
 
   async function getLocation() {
-    console.log(API_KEY);
     const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
     const response = await axios.get(API);
-    console.log(response);
+    const locationObj = response.data[0];
+    setLocation(locationObj);
+
+    console.log(locationObj);
   }
-
-function updateQuery(event) {
-  setSearchQuery(event.target.value);
-}
-
+  function updateQuery(event) {
+    setSearchQuery(event.target.value);
+  }
 
   return (
     <>
-    <input onChange={(updateQuery)} />
-    <button onClick={getLocation}>Explore!</button>
-    <h2>The city is: {location.display_name}</h2>
+      <input onChange={updateQuery} />
+      <button onClick={getLocation}>Explore!</button>
+      <h2>The city is: {location.display_name}</h2>
     </>
   )
 }
-
